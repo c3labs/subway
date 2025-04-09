06b1fcc62159e496204d57a819c6cb7bb016cbfd
Core <core@c3labs.de>
2025-04-09 13:00:46 +0200

    basic accessibility additions v.01

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+	<title>Willkommen bei Radsport Wagner | Aichach</title>
+</svelte:head>
+
 <h1 class=" font-mono ">SUBWAY - Development Server</h1>
 <p>Have a look at <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
