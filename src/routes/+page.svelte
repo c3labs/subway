08e8aa831d04e1ceb1cc7ac17c081f9c2c063ff3
Core <core@c3labs.de>
2025-04-13 20:17:56 +0200

    enhanced:img integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script lang="ts">
+	// import madone from '$lib/assets/images/trek_pone_modone.jpg?enhanced';
+</script>
+
 <svelte:head>
 	<title>Willkommen bei Radsport Wagner | Aichach</title>
 </svelte:head>
 
 <h1 class=" font-mono ">SUBWAY - Development Server</h1>
 <p>Have a look at <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>CURRENTLY TESTING ->> enhanced:img rendering# sizes</p>
+<p>
+	<!-- <enhanced:img src={madone} alt="madone alt text" sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px" /> -->
+	<enhanced:img src="../lib/assets/images/trek_pone_modone.jpg" alt="madone alt text" sizes="min(1280px, 100vw)" />
+</p>
