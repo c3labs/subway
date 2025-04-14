a23a619154dd18ea2466b9d0f008ca4a07cd7e2e
Core <core@c3labs.de>
2025-04-14 15:19:35 +0200

    debug test v.03

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
 <h1 class=" font-mono ">SUBWAY - Development Server</h1>
 <p>Have a look at <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>LAST IMPLEMENTED ->> enhanced:img rendering# format types</p>
-<p>CURRENTLY IMPLEMENTING ->> scrollable:video# size test</p>
+<!-- <p>CURRENTLY IMPLEMENTING ->> scrollable:video# size test</p> -->
 <p>
 	<!-- <enhanced:img src={madone} alt="madone alt text" sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px" /> -->
 	<enhanced:img src="../lib/assets/images/trek_pone_modone.jpg?format=avif;webp" alt="madone alt text" sizes="min(1280px, 100vw)" />
