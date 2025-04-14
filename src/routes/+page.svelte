acdd5942307930a1c670bada60649ced88429322
Core <core@c3labs.de>
2025-04-14 19:35:02 +0200

    debug test v.05 - added $effect rune

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,38 @@
 <script lang="ts">
 	// import madone from '$lib/assets/images/trek_pone_modone.jpg?enhanced';
-	// import scroller from '$lib/assets/video/subway.tester2.mp4';
+	import scroller from '$lib/assets/video/subway.tester2.mp4';
 	// import { run } from 'svelte/legacy';
 
-	// let time = $state(0);
-	// let duration = $state();
+	let time = $state(0);
+	let duration = $state(0);
 
-	// let scrollY = $state(0);
+	let scrollY = $state(0);
 
 	// run(() => {
 	// 	const totalScroll =	document.documentElement.scrollHeight - window.innerHeight;
 	// 	time = duration * (scrollY / totalScroll);
 	// });
+	$effect(() => {
+    	const totalScroll = document.documentElement.scrollHeight - window.innerHeight;
+    	time = duration * (scrollY / totalScroll);
+  	});
 </script>
 
 <svelte:head>
 	<title>SUBWAY - Radsport Wagner | We just ride!</title>
 </svelte:head>
-<!-- <svelte:window bind:scrollY /> -->
+<svelte:window bind:scrollY />
 <h1 class=" font-mono ">SUBWAY - Development Server</h1>
 <p>Have a look at <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>LAST IMPLEMENTED ->> enhanced:img rendering# format types</p>
 <p>CURRENTLY IMPLEMENTING ->> scrollable:video# debug 500 internal error</p>
 <p>
+	<video bind:currentTime={time} bind:duration preload="metadata" muted>
+	<source src={scroller} media="(min-width: 600px)" />
+	<track kind="captions">
+	</video>
+</p>
+<p>
 	<!-- <enhanced:img src={madone} alt="madone alt text" sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px" /> -->
 	<enhanced:img src="../lib/assets/images/trek_pone_modone.jpg?format=avif;webp" alt="madone alt text" sizes="min(1280px, 100vw)" />
-	<!-- <video
-    bind:currentTime={time}
-    bind:duration
-    preload="metadata"
-    mutedtype="video/mp4">
-	<source src={scroller} media="(min-width: 600px)" />
-	</video> -->
 </p>
