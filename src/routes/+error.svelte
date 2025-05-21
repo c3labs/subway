c3df11839cd0a2f1d6cf35e68da95501bc3d85d3
Core <core@c3labs.de>
2025-05-21 23:07:08 +0200

    test: mobile togglable +nav

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/state';
+    import { fade, fly } from "svelte/transition";
+    import { intersect } from '@svelte-put/intersect';
+	import { uiobserver , onIntersect } from "$lib/uiobserver.svelte";
 </script>
 
 
 
-<div class="relative h-screen">
-    <div class="sticky top-0 left-0 z-10 h-screen w-full">
+<div class="relative h-screen" use:intersect={{ threshold: 0.4 }} onintersect={onIntersect} data-uipref="dark">
+    <div class="sticky top-0 left-0 z-10 h-screen w-full bg-neutral-800" in:fade={{duration: 300, delay: 500 }} out:fade>
         <div class=" grid grid-cols-6 gap-2 container place-items-center h-screen">
-            <div class="text-2xl pt-25 font-headline font-semibold text-hero col-span-6">{page.status}: <span class="font-medium">{page.error.message}</span></div>
+            <div class="text-2xl pt-25 font-headline font-bold text-hero col-span-6" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade>{page.status}: <span class="font-light">{page.error.message}</span></div>
         </div>
     </div>
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import logo from '$lib/assets/images/logo/subway.logo.v01.svg?raw';
 	import { uiobserver } from '$lib/uiobserver.svelte';
@@ -11,39 +12,48 @@
 	// 	}
 	// });
 
+	function navigate (navroute: URL) {
+		goto(navroute);
+		console.log('current navSate:', uiobserver.navstate);
+		if(uiobserver.navstate) {
+
+			uiobserver.toggleNav();
+		}
+	}
+
 </script>
 
 <header class="fixed left-0 z-[90] flex flex-col xl:flex-row w-full justify-between px-5 pt-5 pb-5 xl:pb-20 sm:px-10 sm:pt-10 md:px-20 font-menu text-xs backdrop-blur-sm bg-[var(--bg-state-color)]/30 text-[var(--bg-menu-color)] transition-all duration-1000">
 	<div class="corner">
 		<ul class="flex flex-col items-end xl:flex-row gap-x-12">
-			<li class="lg:hidden">
+			<li class="lg:hidden {uiobserver.navstate ? 'opacity-30' : 'opacity-80'}">
 				<button onclick={uiobserver.toggleNav}>{uiobserver.navstate ? '-' : '+'}NAV {uiobserver.uistate}</button>
 			</li>
-			<li aria-current={page.url.pathname === '/bikes' ? 'page' : undefined} class="aria-[current=page]:text-red-600 lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
-				<a href="/bikes">TECH/BIKES</a>
+			<li aria-current={page.url.pathname === '/bikes' ? 'page' : undefined} class="aria-[current=page]:text-hero lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
+				<button onclick={() => navigate('/bikes')} class="cursor-pointer">TECH/BIKES</button>
 				<!-- <button onclick={uiobserver.toggleUi}>UISTATE: {uiobserver.uistate}</button> -->
 			</li>
-			<li aria-current={page.url.pathname === '/projectone' ? 'page' : undefined} class="aria-[current=page]:text-red-600 lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
-				<a href="/projectone">PROJECT ONE</a>
+			<li aria-current={page.url.pathname === '/projectone' ? 'page' : undefined} class="aria-[current=page]:text-hero lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
+				<button onclick={() => navigate("/projectone")} class="cursor-pointer">PROJECT ONE</button>
 			</li>
-			<li aria-current={page.url.pathname === '/leasing' ? 'page' : undefined} class="aria-[current=page]:text-red-600 lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
-				<a href="/leasing">LEASING</a>
+			<li aria-current={page.url.pathname === '/leasing' ? 'page' : undefined} class="aria-[current=page]:text-hero lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
+				<button onclick={() => navigate('/leasing')} class="cursor-pointer">LEASING</button>
 			</li>
 		</ul>
 	</div>
 	<div class="absolute top-5 {uiobserver.navstate ? 'w-25' : 'w-30'} grow left-1/8 -translate-x-1/3 lg:left-1/2 lg:-translate-x-1/2 sm:top-5 sm:w-30 md:top-2 md:w-35 transition-all">
 		<!-- LOGO SECTION HERE -->
-		<a aria-label="subway - we just ride - Logo - back to home" href="/">
+		<button onclick={() => navigate('/')} aria-label="subway - we just ride - Logo - back to home" class="cursor-pointer">
 			{@html logo}
-		</a>
+		</button>
 	</div>
 	<div class="corner">
 		<ul class="flex flex-col items-end xl:flex-row gap-x-12">
-			<li aria-current={page.url.pathname.startsWith('/workbench') ? 'page' : undefined} class="aria-[current=page]:text-red-600 lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
-				<a href="/workbench">+SERVICE/WORKBENCH</a>
+			<li aria-current={page.url.pathname.startsWith('/workbench') ? 'page' : undefined} class="aria-[current=page]:text-hero lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}">
+				<button onclick={() => navigate('/workbench')} class="cursor-pointer">+SERVICE/WORKBENCH</button>
 			</li>
-			<li aria-current={page.url.pathname.startsWith('/info') ? 'page' : undefined} class="aria-[current=page]:text-red-600 lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}" >
-				<a href="/info">+INFORMATION</a>
+			<li aria-current={page.url.pathname.startsWith('/info') ? 'page' : undefined} class="aria-[current=page]:text-hero lg:inline-flex {uiobserver.navstate ? 'inline-flex' : 'hidden'}" >
+				<button onclick={() => navigate('/info')} class="cursor-pointer">+INFORMATION</button>
 			</li>
 		</ul>
 	</div>
