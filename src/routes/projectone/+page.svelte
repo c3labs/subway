8b0cf88657707913625b8bbe0add4f8b12c90693
Core <core@c3labs.de>
2025-07-16 16:01:03 +0200

    test: project one steps layout

--- src/routes/projectone/+page.svelte
+++ src/routes/projectone/+page.svelte
@@ -8,6 +8,8 @@
 	import { animateScroll } from 'svelte-scrollto-element';
 	import { onMount } from "svelte";
 
+    let postep = $state(1);
+
     onMount(() => {
         animateScroll.scrollToTop({ duration: 1, delay: 300 });
         console.log('[    reset scroll position  # projectOne   ] - onMount');
@@ -24,28 +26,46 @@
         <div class="sticky top-0 left-0 z-10 h-[75vh] w-full bg-neutral-800" in:fade={{ duration: 300, delay: 500 }} out:fade>
             <h1 class="container mx-auto px-4 pb-8 z-11 sticky top-32 lg:top-48 xl:top-44 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl" in:fade={{duration: 300, delay: 600 }} out:fade>IN NUR FÜNF STEPS ZU DEINEM COSTOM BIKE | <span class="font-bold"> TREK - PROJECT ONE</span></h1>
             <enhanced:img src="../../lib/assets/images/bikes/projectOne/MY25_Madone_SL_SLR Gen_8.jpg?format=avif;webp" alt="madone alt text" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" />
-            <div class="pb-36 flex h-full w-full flex-col items-stretch justify-center overflow-hidden">
-                <h2 class="container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-800">DU BIST EINZIGARTIG.
+            <div class="absolute h-full w-full top-0">
+                {#if postep === 1}
+                <h2 class="absolute _bottom-30 left-[50%] top-[50%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-800" transition:fade>DU BIST EINZIGARTIG.
                     <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
                         <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">UND DEIN BIKE</span>
                     </span>
                     SOLLTE ES AUCH SEIN.
                 </h2>
+                {/if}
+                {#if postep === 2}
+                <h2 class="absolute _bottom-30 left-[50%] top-[50%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-800" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade>WIR ZEIGEN DIR
+                    <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
+                        <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">WIE DU ZU DEINEM TRAUMBIKE</span>
+                    </span>
+                    KOMMST.
+                </h2>
+                {/if}
             </div>
-            <div class="top-24 _flex _h-full _w-full _flex-col _items-stretch _justify-center _overflow-hidden">
-                <div class="_col-span-6 text-neutral-100" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade>ProjectONE - first section - should show a hero image with headlines H1 first with H2_#1 scrollable to H2_#2</div>
+            <div class="absolute -bottom-[25%] left-[50%] -translate-x-1/2 container mx-auto px-4 text-center">
+                <div class="text-neutral-100 font-headline font-bold text-3xl lg:text-4xl xl:text-5xl" transition:fade>
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 1 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 1}}>1</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 2 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 2}}>2</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 3 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 3}}>3</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 4 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 4}}>4</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 5 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 5}}>5</button>
+                </div>
+                
+                <button class="bg-[var(--bg-state-color)]/30 transition-colors hover:bg-[var(--bg-state-color)]/20 rounded-md px-7 py-3 m-4 backdrop-blur-md cursor-pointer" onclick={() => {postep++}}>NEXT STEP: {postep}</button>
             </div>
             <!-- <div class=" grid grid-cols-6 gap-2 container place-items-center h-screen">
                 
             </div> -->
         </div>
         <div class="top-24 _flex _h-full _w-full _flex-col _items-stretch _justify-center _overflow-hidden">
-            <div class="_col-span-6 text-neutral-800" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade>ProjectONE - first section - should show a hero image with headlines H1 first with H2_#1 scrollable to H2_#2</div>
+            <!-- <div class="_col-span-6 text-red-400" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade>ProjectONE - first section - should show a hero image with headlines H1 first with H2_#1 scrollable to H2_#2</div> -->
         </div>
     </div>
     <div class="relative h-[150vh]" use:intersect={{ threshold: 0.4 }} onintersect={onIntersect} data-uipref="dark" data-uiwidth="50">
         <div class="sticky top-0 left-0 z-10 h-screen w-full bg-neutral-100" out:fade>
-            <h1 class="container mx-auto px-4 pb-8 z-11 sticky top-32 lg:top-48 xl:top-44 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl" in:fade={{duration: 300, delay: 600 }} out:fade>UND SO FUNKTIONIERT'S | <span class="font-bold">STEP 1</span></h1>
+            <h1 class="container mx-auto px-4 pb-8 pt-4 z-11 sticky top-28 lg:top-44 xl:top-40 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl" in:fade={{duration: 300, delay: 600 }} out:fade>UNSERE BIKE VISIONEN - SO INDIVIDUELL WIE IHRE BESITZER | <span class="font-bold">PROJECT ONE GALLERY</span></h1>
             <div class="noscrollbar lg:scroll-smooth_ flex h-full cursor-grab touch-pan-y touch-pinch-zoom select-none overflow-x-scroll max-sm:touch-pan-x snap-x_ snap-proximity_" use:dragscroll id="scrollstage">
                 <div class=" relative h-full shrink-0 overflow-hidden transition-[width] duration-700 ease-in-out {uiobserver.wchange ? 'w-[85vw] lg:w-[60vw]' : 'w-full'}  pb-[15vh] pt-[15vh] snap-start">
                     <div class="text-neutral-900" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade id="stage1">ProjectONE - second section part #1- brief explainer how to get one of these projektONEs implemented as horizontal slider
