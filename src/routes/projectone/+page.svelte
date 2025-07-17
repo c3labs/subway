bc457724f4e034021549a95419229c70d90123ac
Core <core@c3labs.de>
2025-07-17 21:08:21 +0200

    test: project one headlines

--- src/routes/projectone/+page.svelte
+++ src/routes/projectone/+page.svelte
@@ -58,7 +58,7 @@
                 </div>
                 {/if}
                 {#if postep === 1}
-                <h2 class="absolute left-[50%] top-[40%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-800/90 translate-x-0.5 -translate-y-4">Step 1 # </span> Such dir  
+                <h2 class="absolute left-[50%] top-[40%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-900/70 translate-x-0.5 -translate-y-4">Step 1 # </span> Such dir  
                     <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
                         <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">deine Rennmaschine</span>
                     </span>
@@ -71,7 +71,7 @@
                 </div>
                 {/if}
                 {#if postep === 2}
-                <h2 class="absolute left-[50%] top-[70%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-800/90 translate-x-0.5 -translate-y-4">Step 2 # </span> Wähle deinen STYLE aus einer nahezu   
+                <h2 class="absolute left-[50%] top-[70%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-900/70 translate-x-0.5 -translate-y-4">Step 2 # </span> Wähle deinen STYLE aus einer nahezu   
                     <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
                         <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">endlosen Palette</span>
                     </span>
@@ -84,7 +84,7 @@
                 </div>
                 {/if}
                 {#if postep === 3}
-                <h2 class="absolute left-[50%] top-[65%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-800/90 translate-x-0.5 -translate-y-4">Step 3 # </span> Bestücke dein Traumbike mit   
+                <h2 class="absolute left-[50%] top-[65%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-900/70 translate-x-0.5 -translate-y-4">Step 3 # </span> Bestücke dein Traumbike mit   
                     <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
                         <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">TOP Komponenten</span>
                     </span>
@@ -96,15 +96,41 @@
                     Du wählst aus den branchenweit besten Komponenten und passt dein Bike perfekt an deine Wünsche und Ansprüche an.
                 </div>
                 {/if}
+                {#if postep === 4}
+                <h2 class="absolute left-[50%] top-[80%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-900 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-100/70 translate-x-0.5 -translate-y-4">Step 4 # </span> Stimme deine
+                    <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
+                        <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">perfekte Passform</span>
+                    </span>
+                    mit uns ab.
+                </h2>
+                <div class="_bg-neutral-100 text-neutral-500 w-full text-right font-regular text-base rounded-xl absolute left-[50%] _top-[70%] -bottom-[25%] -translate-x-1/2 container mx-auto px-4 pb-6 pt-12" transition:fade>
+                    <!-- Hebe dich mit einem unverwechselbaren Custombike von Project One von der Masse ab. -->
+                    <!-- Jedes TREK Project One Bike wird von einem Team aus talentierten Künstlern und erfahrenen Bikebuildern einzeln von Hand lackiert und aufgebaut.  -->
+                    Wenn du dir schon ein einzigartiges Bike aufbauen läßt, dann sollte es dir auch perfekt passen – von der Kurbelarmlänge über die Kassette bis hin zum Cockpit und zur Sattelstütze. Wir stellen sicher, dass dein Traumbike genauso gut passt wie es aussieht.
+                </div>
+                {/if}
+                {#if postep === 5}
+                <h2 class="absolute left-[50%] top-[70%] -translate-1/2 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-neutral-100 uppercase" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade><span class="absolute text-lg font-bold text-neutral-900/70 translate-x-0.5 -translate-y-4">Step 5 # </span> Jetzt darfst du dich 
+                    <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
+                        <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold">vier Wochen</span>
+                    </span>
+                    auf dein Traumbike freuen.
+                </h2>
+                <div class="_bg-neutral-100 text-neutral-500 w-full text-right font-regular text-base rounded-xl absolute left-[50%] _top-[70%] -bottom-[28%] -translate-x-1/2 container mx-auto px-4 pb-6 pt-12" transition:fade>
+                    <!-- Hebe dich mit einem unverwechselbaren Custombike von Project One von der Masse ab. -->
+                    <!-- Jedes TREK Project One Bike wird von einem Team aus talentierten Künstlern und erfahrenen Bikebuildern einzeln von Hand lackiert und aufgebaut.  -->
+                    Nach nur vier Wochen treffen wir uns hier wieder und machen ein nettes Bild mit dir und deinem Traumbike für unsere Project One Show. Übrigens gibts da auch eine tolle Funktion auf der TREK Website wo man sich seine Konfiguration als link speichern kann falls du dich schon ausführlich mit einem Custom Bike beschäftigt hast.
+                </div>
+                {/if}
             </div>
             <div class="absolute _-bottom-[25%] bottom-2 left-[50%] -translate-x-1/2 container mx-auto px-4 text-center">
                 <!-- <button class="bg-[var(--bg-state-color)]/30 transition-colors hover:bg-[var(--bg-state-color)]/20 rounded-md px-7 py-3 m-4 backdrop-blur-md cursor-pointer" onclick={() => {postep++}}>NEXT STEP: {postep}</button> -->
                 <div class="text-neutral-100 font-headline font-bold text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl" transition:fade>
-                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 1 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 1}}>1</button> - 
-                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 2 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 2}}>2</button> - 
-                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 3 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 3}}>3</button> - 
-                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 4 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 4}}>4</button> - 
-                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 5 ? 'text-neutral-100' : 'text-neutral-600'}" onclick={() => {postep = 5}}>5</button>
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 1 ? 'text-neutral-100 hover:text-neutral-100' : 'text-neutral-900/70 hover:text-neutral-100/40'} active:text-neutral-100/50" onclick={() => {postep = 1}}>1</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 2 ? 'text-neutral-100 hover:text-neutral-100' : 'text-neutral-900/70 hover:text-neutral-100/40'} active:text-neutral-100/50" onclick={() => {postep = 2}}>2</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 3 ? 'text-neutral-100 hover:text-neutral-100' : 'text-neutral-900/70 hover:text-neutral-100/40'} active:text-neutral-100/50" onclick={() => {postep = 3}}>3</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 4 ? 'text-neutral-100 hover:text-neutral-100' : 'text-neutral-900/70 hover:text-neutral-100/40'} active:text-neutral-100/50" onclick={() => {postep = 4}}>4</button> - 
+                    <button class="transition-colors px-2 py-2 cursor-pointer {postep === 5 ? 'text-neutral-100 hover:text-neutral-100' : 'text-neutral-900/70 hover:text-neutral-100/40'} active:text-neutral-100/50" onclick={() => {postep = 5}}>5</button>
                 </div>
             </div>
             <!-- <div class=" grid grid-cols-6 gap-2 container place-items-center h-screen">
@@ -117,7 +143,7 @@
     </div>
     <div class="relative h-[150vh]" use:intersect={{ threshold: 0.4 }} onintersect={onIntersect} data-uipref="dark" data-uiwidth="50">
         <div class="sticky top-0 left-0 z-10 h-screen w-full bg-neutral-100" out:fade>
-            <h1 class="container mx-auto px-4 pb-8 pt-4 z-11 sticky top-28 lg:top-44 xl:top-40 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl" in:fade={{duration: 300, delay: 600 }} out:fade>UNSERE BIKE VISIONEN - SO INDIVIDUELL WIE IHRE BESITZER | <span class="font-bold">PROJECT ONE GALLERY</span></h1>
+            <h1 class="container mx-auto px-4 pb-8 pt-4 z-11 sticky top-28 lg:top-44 xl:top-40 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl" in:fade={{duration: 300, delay: 600 }} out:fade>UNSERE BIKE VISIONEN - SO INDIVIDUELL WIE IHRE BESITZER | <span class="font-bold">PROJECT ONE SHOW</span></h1>
             <div class="noscrollbar lg:scroll-smooth_ flex h-full cursor-grab touch-pan-y touch-pinch-zoom select-none overflow-x-scroll max-sm:touch-pan-x snap-x_ snap-proximity_" use:dragscroll id="scrollstage">
                 <div class=" relative h-full shrink-0 overflow-hidden transition-[width] duration-700 ease-in-out {uiobserver.wchange ? 'w-[85vw] lg:w-[60vw]' : 'w-full'}  pb-[15vh] pt-[15vh] snap-start">
                     <div class="text-neutral-900" in:fly={{ y :100, duration: 300, delay: 500 }} out:fade id="stage1">ProjectONE - second section part #1- brief explainer how to get one of these projektONEs implemented as horizontal slider
