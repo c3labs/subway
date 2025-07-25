ec690381d190aa32d2565717754b4e025fac0664
Core <core@c3labs.de>
2025-07-25 20:16:18 +0200

    test: mix-blend-plus-darker

--- src/routes/bikes/+page.svelte
+++ src/routes/bikes/+page.svelte
@@ -5,40 +5,42 @@
     import { animateScroll } from 'svelte-scrollto-element';
 	import { onMount } from 'svelte';
 
-	let ebikeUlElement: HTMLUListElement | undefined = $state();
+	let mountainUlElement: HTMLUListElement | undefined = $state();
     let racerUlElement: HTMLUListElement | undefined = $state();
     let gravelUlElement: HTMLUListElement | undefined = $state();
-    
 
     interface bikeInfo {
         price: string;
         manufacturer: string;
+        suspension: string;
+        motor: string;
+        watt: string;
         visible: boolean;
     }
 
-	let ebikeIntersectionMap: Record<string, bikeInfo> = $state({
-		"Rail+": { price: "2.899", manufacturer: "Trek", visible: false},
-		"Powerfly+ Gen4": { price: "1.999", manufacturer: "Trek", visible: false},
-		"Powerfly+ FS": { price: "2.699", manufacturer: "Trek", visible: false},
-		"Powerfly EQ": { price: "2.999", manufacturer: "Trek", visible: false},
-		"Powerfly": { price: "3.999", manufacturer: "Trek", visible: false},
-		"Stereo Hybrid One44": { price: "4.999", manufacturer: "Cube", visible: false},
-		"Reaction Hybrid PRO": { price: "3.199", manufacturer: "Cube", visible: false},
-		"Reaction Hybrid RACE": { price: "3.599", manufacturer: "Cube", visible: false},
-		"Reaction Hybrid ALLROAD": { price: "4.199", manufacturer: "Cube", visible: false},
+	let mountainIntersectionMap: Record<string, bikeInfo> = $state({
+		"Rail+": { price: "7.499", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Slash+": { price: "6.999", manufacturer: "Trek", suspension: "FULL - F: 170 - B: 170", motor: "Bosch", watt: "800W", visible: false},
+		"Powerfly+ FS": { price: "5.299", manufacturer: "Trek", suspension: "FULL - F: 130 - B: 120", motor: "Bosch", watt: "800W", visible: false},
+		"Powerfly+ Equipped": { price: "4.799", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "600W", visible: false},
+		"Powerfly FS+ Gen4": { price: "3.999", manufacturer: "Trek", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
+		"Stereo Hybrid One44": { price: "4.999", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
+		"Reaction Hybrid PRO": { price: "3.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
+		"Reaction Hybrid RACE": { price: "3.599", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
+		"Reaction Hybrid ALLROAD": { price: "4.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
 	});
 
     let racerIntersectionMap: Record<string, bikeInfo> = $state({
-        "Madone": { price: "9.899", manufacturer: "Trek", visible: false},
-		"Speed Conceot": { price: "9.899", manufacturer: "Trek", visible: false},
-		"Domane SLR": { price: "10.699", manufacturer: "Trek", visible: false},
-		"Domane+ SLR": { price: "13.999", manufacturer: "Trek", visible: false},
-		"Checkmate SLR": { price: "12.999", manufacturer: "Trek", visible: false},
+        "Madone": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Speed Conceot": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Domane SLR": { price: "10.699", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Domane+ SLR": { price: "13.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Checkmate SLR": { price: "12.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
 	});
 
     let gravelIntersectionMap: Record<string, bikeInfo> = $state({
-        "Checkpoint": { price: "6.899", manufacturer: "Trek", visible: false},
-		"Checkmate SLR": { price: "7.999", manufacturer: "Trek", visible: false},
+        "Checkpoint": { price: "6.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+		"Checkmate SLR": { price: "7.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
 	});
 
     let sectionMap: Record<number, boolean> = $state({
@@ -70,11 +72,13 @@
     })
 </script>
 
-<div class="relative h-screen" use:intersect={{ threshold: 0.4 }} onintersect={onIntersect} data-uipref="dark">
-    <div class="sticky top-0 left-0 z-10 h-screen w-full">
+<div class="relative h-[300vh] _h-screen" use:intersect={{ threshold: 0.4 }} onintersect={onIntersect} data-uipref="dark">
+    <div class="sticky top-0 left-0 z-10 h-screen w-full bg-hero_">
         <h1 class="container mx-auto px-4 pb-8 z-11 sticky top-32 lg:top-48 xl:top-44 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl text-shadow-sm" in:fade={{duration: 300, delay: 600 }} out:fade>WIR HABEN FÜR JEDEN DEINER TRAILS DAS PASSENDE BIKE | <span class="font-bold"> TECH/BIKES</span></h1>
         {#if sectionStep === 0}
-            <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_4.jpg?format=avif;webp" alt="Bikes - Hero Shot - Übersicht" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" transition:fade />
+        <div class=" ">
+            <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_4.jpg?format=avif;webp" alt="Bikes - Hero Shot - Übersicht" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center _mix-blend-multiply brightness-110 contrast-115" in:fade={{duration: 300}} out:fade />
+        </div>
         {/if}
         {#if sectionStep === 1}
             <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_1.jpg?format=avif;webp" alt="Bikes - Hero Shot - E-Bikes" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" transition:fade />
@@ -93,17 +97,17 @@
                     <button class="bg-[var(--bg-state-color)]/30 transition-colors hover:bg-[var(--bg-state-color)]/20 rounded-md px-7 py-3 m-4 backdrop-blur-md cursor-pointer text-neutral-300" onclick={() => { setSection(3) }}> GRAVELBIKES </button>
                 </p>
 
-                <ul class="{sectionMap[1] ? 'max-h-[50vh]' : 'max-h-[1px]'} w-full space-y-4 overflow-hidden overflow-y-auto p-4 col-span-6 transition-all duration-500" bind:this={ebikeUlElement} >
-                    {#each Object.keys(ebikeIntersectionMap) as key (key)}
+                <ul class="{sectionMap[1] ? 'max-h-[50vh]' : 'max-h-[1px]'} w-full space-y-4 overflow-hidden overflow-y-auto p-4 col-span-6 transition-all duration-500" bind:this={mountainUlElement} >
+                    {#each Object.keys(mountainIntersectionMap) as key (key)}
                         <!-- {#key intersectionMap[key]} -->
                             <li
-                                class=" odd:bg-neutral-100 even:bg-hero h-[200px] marker:content-none transition-opacity {!ebikeIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
-                                use:intersect={{ threshold: 0.99, root: ebikeUlElement }}
+                                class=" odd:bg-neutral-100 even:bg-hero even:mix-blend-darken h-[200px] marker:content-none transition-opacity {!mountainIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
+                                use:intersect={{ threshold: 0.99, root: mountainUlElement }}
                                 onintersect={(event) => (
-                                    ebikeIntersectionMap[key].visible = event.detail.entries[0].isIntersecting,
+                                    mountainIntersectionMap[key].visible = event.detail.entries[0].isIntersecting,
                                     console.log(event.detail)
                                     )}
-                            >Bike Detail: {key} | {ebikeIntersectionMap[key].price} | {ebikeIntersectionMap[key].manufacturer}</li>
+                            >Bike Detail: {key} | {mountainIntersectionMap[key].price} | {mountainIntersectionMap[key].manufacturer}</li>
                         <!-- {/key} -->
                     {/each}
                 </ul>
@@ -111,7 +115,7 @@
                     {#each Object.keys(racerIntersectionMap) as key (key)}
                         <!-- {#key intersectionMap[key]} -->
                             <li
-                                class=" odd:bg-neutral-100 even:bg-hero h-[200px] marker:content-none transition-opacity {!racerIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
+                                class=" odd:bg-neutral-100 even:bg-hero even:mix-blend-plus-darker h-[200px] marker:content-none transition-opacity {!racerIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
                                 use:intersect={{ threshold: 0.99, root: racerUlElement }}
                                 onintersect={(event) => (
                                     racerIntersectionMap[key].visible = event.detail.entries[0].isIntersecting
@@ -124,7 +128,7 @@
                     {#each Object.keys(gravelIntersectionMap) as key (key)}
                         <!-- {#key intersectionMap[key]} -->
                             <li
-                                class=" odd:bg-neutral-100 even:bg-hero h-[200px] marker:content-none transition-opacity {!gravelIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
+                                class=" odd:bg-neutral-100 even:bg-hero even:mix-blend-darken h-[200px] marker:content-none transition-opacity {!gravelIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100"'} duration-500"
                                 use:intersect={{ threshold: 0.99, root: gravelUlElement }}
                                 onintersect={(event) => (
                                     gravelIntersectionMap[key].visible = event.detail.entries[0].isIntersecting
