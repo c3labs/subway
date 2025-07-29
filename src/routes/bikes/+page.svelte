73172664a0e462f85c805857dfaffdcd02369de9
Core <core@c3labs.de>
2025-07-29 13:54:49 +0200

    test: viewportWidthDiv

--- src/routes/bikes/+page.svelte
+++ src/routes/bikes/+page.svelte
@@ -12,6 +12,7 @@
     let scrollY = $state(0);
 	let scrollQuot = $state(0);
     let scrollYWidth: number = $state(0);
+    let viewportWidthDiv: number = $state(0);
 
 
     interface bikeInfo {
@@ -79,28 +80,35 @@
 	$effect(() => {
 		const techstage: HTMLElement | null = document.getElementById("techstage");
 		const scrollYstage: HTMLElement | null = document.getElementById("scrollyStage");
-        // @ts-ignore
+
+        const viewport = window.visualViewport;
+        
+        if (!techstage || !scrollYstage) return;
         scrollYWidth = scrollYstage?.scrollWidth - scrollYstage?.clientWidth;
-        // scrollYWidth = scrollYstage?.scrollWidth;
 
         // @ts-ignore
-		var techOverflow = document.body.scrollHeight - techstage.scrollHeight;
-		if (techOverflow > window.innerHeight) {
-			techOverflow = window.innerHeight;
-		}
+		// var techOverflow = document.body.scrollHeight - techstage.scrollHeight;
+		// if (techOverflow > window.innerHeight) {
+		// 	techOverflow = window.innerHeight;
+		// }
 
-		// @ts-ignore
-		// var maxScrollY = techstage?.scrollHeight - window.innerHeight + techOverflow;
-        var maxScrollY = techstage?.scrollHeight - window.innerHeight;
+        var maxScrollY = techstage.scrollHeight - window.innerHeight;
 
 		scrollQuot = 1 / maxScrollY * scrollY;
 
         scrollYstage?.scroll({
             left: (scrollYWidth * scrollQuot),
-            behavior: "instant",
+            behavior: "auto",
         });
+
+        const viewportHeight = viewport?.height ?? window.innerHeight;
+        if( window.innerHeight > viewportHeight ) {
+            viewportWidthDiv = window.innerHeight - viewportHeight;
+        };
+
         // console.log(scrollYWidth);
         // console.log(scrollQuot);
+        console.log('[    viewport height   # bikes   ] - ', viewportHeight, ' | viewportWidthDiv: ', viewportWidthDiv);
 
 	});
 
@@ -128,15 +136,15 @@
         {#if sectionStep === 3}
             <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_3.jpg?format=avif;webp" alt="Bikes - Hero Shot - Gravelbikes" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" transition:fade />
         {/if}
-        <div class="absolute left-0 bottom-0 overflow-hidden  h-40 lg:h-80 w-full">
+        <div class="absolute left-0 bottom-0 overflow-hidden  h-40 lg:h-80 w-full" style="transform: translateY(-{viewportWidthDiv}px);">
             <div class="absolute top-0 left-0 bg-hero w-full h-full mix-blend-multiply z-10 -rotate-6_ scale-60_"></div>
             <div class="absolute flex gap-20 lg:gap-40 w-full h-40 lg:h-80 top-1/2_ bottom-0 overflow-hidden overflow-x-scroll_ noscrollbar" id="scrollyStage">    
-                <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase tracking-tighter z-11">Mountain</button>
+                <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase tracking-tighter z-11 pl-20">Mountain</button>
                 <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.05em] z-11">Road</button>
                 <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.03em] z-11">Gravel</button>
                 <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.03em] z-11">City</button>
                 <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.03em] z-11">Trekking</button>
-                <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.03em] z-11">Kids</button>
+                <button class="font-headline font-bold text-neutral-50 text-8xl lg:text-[240px] uppercase -tracking-[.03em] z-11 pr-20">Kids</button>
             </div>
         </div>
         <div class="container mx-auto grid grid-cols-6 gap-2 place-items-center h-screen">
