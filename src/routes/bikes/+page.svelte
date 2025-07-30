4809f01e9a6b37ffbe936369fbe633f04faff6ea
Core <core@c3labs.de>
2025-07-30 22:42:32 +0200

    test: bike section v.03

--- src/routes/bikes/+page.svelte
+++ src/routes/bikes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+    import mountainPic_1 from '$lib/assets/images/bikes/heros/bikeHero_0.jpg?enhanced'
+
 	import { fade, fly } from "svelte/transition";
     import { intersect } from '@svelte-put/intersect';
 	import { uiobserver , onIntersect } from "$lib/uiobserver.svelte";
+    import { dragscroll } from '@svelte-put/dragscroll';
     import { animateScroll } from 'svelte-scrollto-element';
 	import { onMount } from 'svelte';
+	// import type { EnhancedImgAttributes } from '@sveltejs/enhanced-img';
 
 	let mountainUlElement: HTMLUListElement | undefined = $state();
     let racerUlElement: HTMLUListElement | undefined = $state();
@@ -20,32 +24,33 @@
         suspension: string;
         motor: string;
         watt: string;
+        imgpath: string;
         visible: boolean;
     }
 
 	let mountainIntersectionMap: Record<string, bikeInfo> = $state({
-		"Rail+": { price: "7.499", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Slash+": { price: "6.999", manufacturer: "Trek", suspension: "FULL - F: 170 - B: 170", motor: "Bosch", watt: "800W", visible: false},
-		"Powerfly+ FS": { price: "5.299", manufacturer: "Trek", suspension: "FULL - F: 130 - B: 120", motor: "Bosch", watt: "800W", visible: false},
-		"Powerfly+ Equipped": { price: "4.799", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "600W", visible: false},
-		"Powerfly FS+ Gen4": { price: "3.999", manufacturer: "Trek", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
-		"Stereo Hybrid One44": { price: "4.999", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
-		"Reaction Hybrid PRO": { price: "3.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
-		"Reaction Hybrid RACE": { price: "3.599", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
-		"Reaction Hybrid ALLROAD": { price: "4.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", visible: false},
+		"Rail+": { price: "7.499", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "bikeHero_0.jpg", visible: false},
+		"Slash+": { price: "6.999", manufacturer: "Trek", suspension: "FULL - F: 170 - B: 170", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Powerfly+ FS": { price: "5.299", manufacturer: "Trek", suspension: "FULL - F: 130 - B: 120", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Powerfly+ Equipped": { price: "4.799", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "600W", imgpath: "", visible: false},
+		"Powerfly FS+ Gen4": { price: "3.999", manufacturer: "Trek", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Stereo Hybrid One44": { price: "4.999", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Reaction Hybrid PRO": { price: "3.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Reaction Hybrid RACE": { price: "3.599", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Reaction Hybrid ALLROAD": { price: "4.199", manufacturer: "Cube", suspension: "FULL - F: xxx - B: xxx", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
 	});
 
-    let racerIntersectionMap: Record<string, bikeInfo> = $state({
-        "Madone": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Speed Conceot": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Domane SLR": { price: "10.699", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Domane+ SLR": { price: "13.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Checkmate SLR": { price: "12.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+    let roadIntersectionMap: Record<string, bikeInfo> = $state({
+        "Madone": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Speed Conceot": { price: "9.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Domane SLR": { price: "10.699", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Domane+ SLR": { price: "13.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Checkmate SLR": { price: "12.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
 	});
 
     let gravelIntersectionMap: Record<string, bikeInfo> = $state({
-        "Checkpoint": { price: "6.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
-		"Checkmate SLR": { price: "7.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", visible: false},
+        "Checkpoint": { price: "6.899", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
+		"Checkmate SLR": { price: "7.999", manufacturer: "Trek", suspension: "FULL - F: 160 - B: 160", motor: "Bosch", watt: "800W", imgpath: "", visible: false},
 	});
 
     let sectionMap: Record<number, boolean> = $state({
@@ -97,7 +102,7 @@
 		scrollQuot = 1 / maxScrollY * scrollY;
         var scrollSection = Math.floor(scrollQuot * 100);
 
-        if (scrollSection >= 0 && scrollSection < 7) {
+        if (scrollSection >= -1 && scrollSection < 7) {
             if (prevStep != 0) {
                 setSection(0);
             }
@@ -148,9 +153,7 @@
     <div class="sticky top-0 left-0 z-10 h-screen w-full bg-hero_">
         <h1 class="container mx-auto px-4 pb-8 z-11 sticky top-32 lg:top-48 xl:top-44 font-extralight font-headline text-neutral-800 text-sm lg:text-base xl:text-xl text-shadow-sm" in:fade={{duration: 300, delay: 600 }} out:fade>WIR HABEN FÜR JEDEN DEINER TRAILS DAS PASSENDE BIKE | <span class="font-bold"> TECH/BIKES</span></h1>
         {#if sectionStep === 0}
-        <div class=" ">
             <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_4.jpg?format=avif;webp" alt="Bikes - Hero Shot - Übersicht" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center _mix-blend-multiply brightness-120 contrast-115" in:fade={{duration: 300}} out:fade />
-        </div>
         {/if}
         {#if sectionStep === 1}
             <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_1.jpg?format=avif;webp" alt="Bikes - Hero Shot - E-Bikes" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" transition:fade />
@@ -161,10 +164,9 @@
         {#if sectionStep === 3}
             <enhanced:img src="/src/lib/assets/images/bikes/heros/bikeHero_3.jpg?format=avif;webp" alt="Bikes - Hero Shot - Gravelbikes" sizes="min(1280px, 100vw)" class="absolute left-0 top-0 -z-50 h-full w-full overflow-hidden object-cover object-center" transition:fade />
         {/if}
-        <!-- <div class="absolute left-0 bottom-0 overflow-hidden h-40 lg:h-80 w-full" style="transform: translateY(-{viewportWidthDiv}px);"> -->
-        <div class="absolute left-0 {sectionStep === 0 ? 'bottom-60' : 'bottom-0'} overflow-hidden h-40 lg:h-80 w-full transition-all duration-1000 ease-in-out">
+        <div class="absolute left-0 {sectionStep === 0 ? 'bottom-1/4' : 'bottom-0'} overflow-hidden h-40 lg:h-80 w-full transition-all duration-1000 ease-in-out">
             {#if sectionStep === 0}
-                <h2 class="relative left-[30%] top-[45%] lg:top-10 -translate-1/2 z-12 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-white uppercase _text-shadow-lg" in:fly={{ y:50, duration: 300, delay: 600 }} out:fade >WELCHEN
+                <h2 class="relative left-[50%] lg:left-[30%] top-[45%] lg:top-10 -translate-1/2 z-12 container mx-auto px-4 landscape:max-lg:translate-y-6 text-right lg:text-center font-headline font-extralight text-3xl/12 landscape:max-lg:text-3xl md:text-4xl text-white uppercase _text-shadow-lg" in:fly={{ y:50, duration: 300, delay: 600 }} out:fade >WELCHEN
                     <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-hero/70">
                         <span class="relative text-white text-4xl landscape:max-lg:text-4xl md:text-5xl font-bold text-shadow-none">TRAIL</span>
                     </span>
@@ -189,38 +191,45 @@
                     <button class="bg-[var(--bg-state-color)]/30 transition-colors hover:bg-[var(--bg-state-color)]/20 rounded-md px-7 py-3 m-4 backdrop-blur-md cursor-pointer text-neutral-300" onclick={() => { setSection(3) }}> GRAVELBIKES </button>
                 </p> -->
                 {#if sectionStep === 1}
-                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={mountainUlElement} >
+                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={mountainUlElement} use:dragscroll >
                         {#each Object.keys(mountainIntersectionMap) as key (key)}
                             <!-- {#key intersectionMap[key]} -->
                                 <li
                                     class="bg-neutral-100 h-[400px] min-w-[400px] marker:content-none transition-all {!mountainIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100'} duration-500"
-                                    use:intersect={{ threshold: 0.6, root: mountainUlElement }}
+                                    use:intersect={{ threshold: 0.4, root: mountainUlElement }}
                                     onintersect={(event) => (
                                         mountainIntersectionMap[key].visible = event.detail.entries[0].isIntersecting,
                                         console.log(event.detail)
                                         )}
-                                >Bike Detail: {key} | {mountainIntersectionMap[key].price} | {mountainIntersectionMap[key].manufacturer}</li>
+                                >
+                                {#if mountainIntersectionMap[key].imgpath != ""}
+                                        image: {mountainIntersectionMap[key].imgpath} 
+                                    <img src="{'/src/lib/assets/images/bikes/heros/' + mountainIntersectionMap[key].imgpath}" alt="Bikes - Hero Shot - Übersicht" class="_" draggable="false" />
+                                    <!-- <enhanced:img alt="Bikes - Hero Shot - Übersicht" src={mountainPic_1}  class="_" draggable="false" /> -->
+                                {/if}
+                                    <div>Detail: {key} | {mountainIntersectionMap[key].price} | {mountainIntersectionMap[key].manufacturer}</div>
+                            </li>
                             <!-- {/key} -->
                         {/each}
                     </ul>
                 {/if}
                 {#if sectionStep === 2}
-                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={racerUlElement} >
-                        {#each Object.keys(racerIntersectionMap) as key (key)}
+                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={racerUlElement} use:dragscroll >
+                        {#each Object.keys(roadIntersectionMap) as key (key)}
                             <!-- {#key intersectionMap[key]} -->
                                 <li
-                                    class="bg-neutral-100 h-[400px] min-w-[400px] marker:content-none transition-all {!racerIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100'} duration-500"
+                                    class="bg-neutral-100 h-[400px] min-w-[400px] marker:content-none transition-all {!roadIntersectionMap[key].visible ? 'opacity-0' : 'opacity-100'} duration-500"
                                     use:intersect={{ threshold: 0.6, root: racerUlElement }}
                                     onintersect={(event) => (
-                                        racerIntersectionMap[key].visible = event.detail.entries[0].isIntersecting
+                                        roadIntersectionMap[key].visible = event.detail.entries[0].isIntersecting
                                         )}
-                                >Bike Detail: {key} | {racerIntersectionMap[key].price} | {racerIntersectionMap[key].manufacturer}</li>
+                                >Bike Detail: {key} | {roadIntersectionMap[key].price} | {roadIntersectionMap[key].manufacturer}</li>
                             <!-- {/key} -->
                         {/each}
                     </ul>
                 {/if}
                 {#if sectionStep === 3}
-                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={gravelUlElement} >
+                    <ul class="max-h-[50vh] w-full space-y-4 flex gap-10 overflow-hidden overflow-x-auto p-4 col-span-6 transition-all duration-500" bind:this={gravelUlElement} use:dragscroll >
                         {#each Object.keys(gravelIntersectionMap) as key (key)}
                             <!-- {#key intersectionMap[key]} -->
                                 <li
